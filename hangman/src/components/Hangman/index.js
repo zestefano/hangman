b9db9f1af8357db3a7dd6389142f243f45d19fb8
zachery estefano <zacheryestefano@gmail.com>
2022-06-05 03:57:24 -0400

    first image working

--- hangman/src/components/Hangman/index.js
+++ hangman/src/components/Hangman/index.js
@@ -5,11 +5,26 @@ import './csvUpload.css'
 
 
 const Upload = () => {
+    const img0 = 'https://res.cloudinary.com/zaf/image/upload/v1654414896/hangman/Screen_Shot_2022-06-05_at_3.35.23_AM_fse8w7.png';
+    const img1 = 'https://res.cloudinary.com/zaf/image/upload/v1654414895/hangman/Screen_Shot_2022-06-05_at_3.35.32_AM_mi52dy.png';
+    const img2 = 'https://res.cloudinary.com/zaf/image/upload/v1654414895/hangman/Screen_Shot_2022-06-05_at_3.35.42_AM_ktfcn9.png';
+    const img3 = 'https://res.cloudinary.com/zaf/image/upload/v1654414895/hangman/Screen_Shot_2022-06-05_at_3.35.50_AM_mo1kyd.png';
+    const img4 = 'https://res.cloudinary.com/zaf/image/upload/v1654414895/hangman/Screen_Shot_2022-06-05_at_3.35.58_AM_fjh0b9.png';
+    const img5 = 'https://res.cloudinary.com/zaf/image/upload/v1654414895/hangman/Screen_Shot_2022-06-05_at_3.36.07_AM_vsdxul.png';
+    const img6 = 'https://res.cloudinary.com/zaf/image/upload/v1654414895/hangman/Screen_Shot_2022-06-05_at_3.36.17_AM_dso0be.png';
+   
+   
     const [hover, setHover] = useState(false);
     const [words, setWords] = useState([]);
-    const [selectedWord, setSelectedWord] =  useState(words[0])
+    const [selectedWord, setSelectedWord] =  useState('');
+    const [guessed, setGuessed] = useState([]);
+    const [turn, setTurn] = useState(0);
+    const [images, setImages] = useState([img0, img1, img2, img3, img4, img5, img6])
+    const maxTurns = 6;
+    const gameOver = turn >= maxTurns;
     // let selectedWord = words[Math.floor(Math.random() * words.length)];
 
+    
 
     const playAgain = () => {
         const random = Math.floor(Math.random() * words.length);
@@ -41,13 +56,15 @@ const Upload = () => {
                         const res = parse(text)
                         const csvData = res.data.flat()
                         setWords(csvData)
-                        setSelectedWord(csvData[0])
+                        setSelectedWord(csvData[Math.floor(Math.random() * csvData.length)])
                         console.log(csvData)
                     })
                 }}
             >
                 DROP HERE
             </div>
+                <p>wrong guesses {turn} of {maxTurns}</p>
+                <img src={images[turn]} />
                 <Word selectedWord={selectedWord}/>
                 <button onClick={playAgain}>New Game</button>
         </div>
